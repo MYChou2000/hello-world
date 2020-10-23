f08cb5b08f06255e8ac1c14b3ab60009f9d63565
MYChou2000 <53605946+MYChou2000@users.noreply.github.com>
2020-10-23 16:12:26 +0800

    Update Good.cpp

--- Good.cpp
+++ Good.cpp
@@ -6,4 +6,6 @@ int main() {
     cout << "2nd line" << endl;
     cout << "3rd line" << endl;
     cout << "Add a line on GitHub" << endl;
+    cout << "Minic the operatoin of A and B." << endl;
+    
 }
