bca76f358687ae44c276037b3714f3b0c14511e5
MYChou2000 <53605946+MYChou2000@users.noreply.github.com>
2020-10-28 18:32:39 +0800

    Update 4_6_back_to_a_time_stamp.cpp

--- 4_6_back_to_a_time_stamp.cpp
+++ 4_6_back_to_a_time_stamp.cpp
@@ -5,6 +5,7 @@ int main() {
     cout << "time stamp 1." << endl;
     cout << "I go to fetch a coke." << endl;
     cout << "I go out to buy lunch box." << enld;
+    cout << "I go out to dump the garbage." << endl;
     
     
     
