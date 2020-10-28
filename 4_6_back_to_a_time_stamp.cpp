b0dad86316f9b1cfd36c443e442d7887b2626fcc
MYChou2000 <53605946+MYChou2000@users.noreply.github.com>
2020-10-28 21:04:22 +0800

    Update 4_6_back_to_a_time_stamp.cpp

--- 4_6_back_to_a_time_stamp.cpp
+++ 4_6_back_to_a_time_stamp.cpp
@@ -4,11 +4,7 @@ using namespace std;
 int main() {
     cout << "time stamp 1." << endl;
     cout << "I go to fetch a coke." << endl;
-    cout << "I go out to buy lunch box." << enld;
-    cout << "I go out to dump the garbage." << endl;
-    cout << "I go to the door to fetch a pizza from UBER eat." << endl;
-    cout << "Push to the GitHub." << endl;
-    
+
     
     
 }
