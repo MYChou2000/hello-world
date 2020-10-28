34fdd9044c5be059d3a7301abf61c0a1a8e1c23a
MYChou2000 <53605946+MYChou2000@users.noreply.github.com>
2020-10-28 17:47:49 +0800

    Update 4_4_pull_without_committing.cpp

--- 4_4_pull_without_committing.cpp
+++ 4_4_pull_without_committing.cpp
@@ -1,6 +1,6 @@
 // merge a temb branch to a master branch
 #include <iostream>
-usingn namespace std;
+using namespace std;
 
 int main() {
     cout << "1 create a cpp file and push to Github" << endl;
