3513f36be08df9d0663033bf344c0e763628d082
MYChou2000 <53605946+MYChou2000@users.noreply.github.com>
2020-10-28 17:27:13 +0800

    Update 4_4_pull_without_committing_p2.cpp

--- 4_4_pull_without_committing_p2.cpp
+++ 4_4_pull_without_committing_p2.cpp
@@ -5,4 +5,6 @@ int main() {
     cout << "2nd test the pull without committing." << endl;
     cout << "1. I created a file on PC." << endl;
     
+    cout << "3. It was edited by myself on PC." << endl;
+    
 }
