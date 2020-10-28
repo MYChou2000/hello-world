3e89ffa2f5781dbc50630fc4a31ff652bf54f3d9
MYChou2000 <53605946+MYChou2000@users.noreply.github.com>
2020-10-28 15:41:49 +0800

    Update 4_4_pull_without_committing.cpp

--- 4_4_pull_without_committing.cpp
+++ 4_4_pull_without_committing.cpp
@@ -4,6 +4,6 @@ usingn namespace std;
 
 int main() {
     cout << "1 create a cpp file and push to Github" << endl;
-    
+    cout << "2 after pulling, edit this file on the Github." << endl;
     
 }
