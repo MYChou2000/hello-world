ddaf734826a1a867a1d44f114e1318a30e27ef10
MYChou2000 <53605946+MYChou2000@users.noreply.github.com>
2020-10-28 21:07:40 +0800

    Update 4_6_back_to_a_time_stamp.cpp

--- 4_6_back_to_a_time_stamp.cpp
+++ 4_6_back_to_a_time_stamp.cpp
@@ -6,6 +6,7 @@ int main() {
     cout << "I go to fetch a coke." << endl;
     cout << "I go out to buy lunch box." << enld;
     cout << "I go out to dump the garbage." << endl;
- 
-   
+    cout << "I go to the door to fetch a pizza from UBER eat." << endl;
+    cout << "Push to the GitHub." << endl;
+     
 }
